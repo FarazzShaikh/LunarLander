c80db6496f0f63fe1bd545af4fae97a3e316ba75
Farhan Alvi <farhanalvi005@gmail.com>
2020-09-27 19:41:00 +0600

    Remove old code

--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,6 @@ const app = express()
 
 const Game = require('../client/js/main')
 
-const webpack = require('webpack')
-const webpackDevMiddleware = require('webpack-dev-middleware')
-const webpackDevConfig = require('../../webpack.dev.js');
-const compiler = webpack(webpackDevConfig)
-
 const port = process.env.PORT || 5000
 const server = app.listen(port)
 console.log(`Listening on port :${port}`)
