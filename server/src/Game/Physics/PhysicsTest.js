0c6ae5256f04e4a3dbd112562a9ca6103686d978
Faraz Shaikh <farazzshaikh@gmail.com>
2020-10-06 05:40:19 +0530

    Added Abstract Physics Class

---
+++ server/src/Game/Physics/PhysicsTest.js
@@ -0,0 +1,14 @@
+import Physics from "./_Physics";
+
+export default class PhysicsTest extends Physics{
+    calculateForce(dt) {
+        return {
+            x: 0.1,
+            y: 0
+        }
+    }
+
+    calculateTorque(dt) {
+        return 0
+    }
+}
