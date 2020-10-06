da83cad4ded81bc835ee59a3b0e33046f9ad9894
TheChosenSir <71703249+TheChosenSir@users.noreply.github.com>
2020-10-06 12:10:48 +0400

    Update server/src/Game/Game.js
    Co-authored-by: Faraz Shaikh <farazzshaikh@gmail.com>

--- server/src/Game/Game.js
+++ server/src/Game/Game.js
@@ -86,7 +86,7 @@ export default class Game {
         const dt = this._tick() / 1000
         Object.values(this.players).forEach(player => {
             player.socket.emit(EVENTS.SERVER_TICK, dt)
-            const pchange = player.update(dt)
+            const doesPlayerUpdate = player.update(dt)
 
             //Checks for player position changes and updates their position
             if(pchange){ 
