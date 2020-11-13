9af6f324f617e863caf1ed3cc39444ec4ebc5d80
TheChosenSir <71703249+TheChosenSir@users.noreply.github.com>
2020-11-13 16:02:37 +0400

    Added muteMe and mutePage functions to TitleScreen

--- client/src/TitleScreen/TitleScreen.js
+++ client/src/TitleScreen/TitleScreen.js
@@ -182,3 +182,14 @@ async function setUser(body) {
 	});
 	return response;
 }
+
+// Mute a singular HTML5 element
+function muteMe(elem) {
+    elem.muted = true;
+    elem.pause();
+}
+
+// Try to mute all video and audio elements on the page
+function mutePage() {
+    document.querySelectorAll("video, audio").forEach( elem => muteMe(elem) );
+}
