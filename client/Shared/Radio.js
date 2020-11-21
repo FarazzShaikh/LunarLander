77ab37b880acaf772cf6efb0a5d450325ea07312
Faraz Shaikh <farazzshaikh@gmail.com>
2020-11-21 12:51:44 +0530

    Hosts Radio on Dropbox

--- client/Shared/Radio.js
+++ client/Shared/Radio.js
@@ -1,4 +1,3 @@
-import { getSong } from '../src/Assets/Sounds/Radio/Before the night/BeforeTheNight';
 import Audio from '../src/Engine/Audio';
 
 export default class Radio {
@@ -9,17 +8,18 @@ export default class Radio {
 
 		document.body.appendChild(node);
 
-		this.songIndex = 1;
+		this.songIndex = 10;
 
 		this.audio = new Audio('', 1);
-		// getSong(this.songIndex, songTable.HOME.tracks[this.songIndex - 1]).then(
-		// 	(s) => {
-		// 		this.audio.setSrc(s.default);
-		// 		setTimeout(() => {
-		// 			this.play();
-		// 		}, 1000);
-		// 	}
-		// );
+		this.audio.sound.crossOrigin = 'anonymous';
+		this.audio.setSrc(sanitizeLink(songTable.HOME.url[this.songIndex - 1]));
+		this.audio.play();
+
+		this.audio.sound.addEventListener('ended', () => {
+			this.stop();
+			this.next();
+			this.play();
+		});
 
 		let audioContext = new AudioContext();
 
@@ -75,33 +75,23 @@ export default class Radio {
 	}
 
 	next() {
-		// this.songIndex++;
-		// if (this.songIndex === 12) this.songIndex = 1;
-		// getSong(this.songIndex, songTable.HOME.tracks[this.songIndex - 1]).then(
-		// 	(s) => {
-		// 		this.audio.setSrc(s.default);
-		// 		setTimeout(() => {
-		// 			this.play();
-		// 		}, 1000);
-		// 	}
-		// );
-		// const data = this.getSongData();
-		// if (this.onSongChange) this.onSongChange(data);
+		this.songIndex++;
+		if (this.songIndex === 12) this.songIndex = 1;
+
+		this.audio.setSrc(sanitizeLink(songTable.HOME.url[this.songIndex - 1]));
+
+		const data = this.getSongData();
+		if (this.onSongChange) this.onSongChange(data);
 	}
 
 	previous() {
-		// this.songIndex--;
-		// if (this.songIndex === 0) this.songIndex = 11;
-		// getSong(this.songIndex, songTable.HOME.tracks[this.songIndex - 1]).then(
-		// 	(s) => {
-		// 		this.audio.setSrc(s.default);
-		// 		setTimeout(() => {
-		// 			this.play();
-		// 		}, 1000);
-		// 	}
-		// );
-		// const data = this.getSongData();
-		// if (this.onSongChange) this.onSongChange(data);
+		this.songIndex--;
+		if (this.songIndex === 0) this.songIndex = 11;
+
+		this.audio.setSrc(sanitizeLink(songTable.HOME.url[this.songIndex - 1]));
+
+		const data = this.getSongData();
+		if (this.onSongChange) this.onSongChange(data);
 	}
 }
 
@@ -122,5 +112,23 @@ var songTable = {
 			'Sun',
 			'Sleep',
 		],
+		url: [
+			`https://www.dropbox.com/s/7wapxmlov3j2rj3/1-%20We%27re%20Finally%20Landing.mp3?dl=0`,
+			`https://www.dropbox.com/s/oy6wsxs9z4vada1/2-%20Synchronize.mp3?dl=0`,
+			`https://www.dropbox.com/s/2nd286ybzu4ssni/3-%20Overflow.mp3?dl=0`,
+			`https://www.dropbox.com/s/fwyv7ktcvd9hlbz/4-%20Without%20A%20Sound.mp3?dl=0`,
+			`https://www.dropbox.com/s/oa4ld7obq74b42m/5-%20Above%20All.mp3?dl=0`,
+			`https://www.dropbox.com/s/l83e9h92h99gkse/6-%20Pyxis.mp3?dl=0`,
+			`https://www.dropbox.com/s/6wn831srlhs9902/7-%20Before%20The%20Night.mp3?dl=0`,
+			`https://www.dropbox.com/s/l2wc3k8m6fa91o5/8-%20If%20I%27m%20Wrong.mp3?dl=0`,
+			`https://www.dropbox.com/s/l2wc3k8m6fa91o5/8-%20If%20I%27m%20Wrong.mp3?dl=0`,
+			`https://www.dropbox.com/s/xxp4fjyfqyu81mj/10-%20Sun.mp3?dl=0`,
+			`https://www.dropbox.com/s/p3657xnj0pbznnk/11-%20Sleep.mp3?dl=0`,
+		],
 	},
 };
+
+function sanitizeLink(link) {
+	const sub = link.substring(0, link.length - 5);
+	return sub.replace('www', 'dl');
+}
